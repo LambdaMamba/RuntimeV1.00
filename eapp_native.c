1e9fc3d483858b65bd4c82e0436e155da8194977
LambdaMamba <57588612+LambdaMamba@users.noreply.github.com>
2021-06-09 04:24:03 +0900

    New Eapp

--- eapp_native.c
+++ eapp_native.c
@@ -1,7 +1,3 @@
-//******************************************************************************
-// Copyright (c) 2018, The Regents of the University of California (Regents).
-// All Rights Reserved. See LICENSE for license details.
-//------------------------------------------------------------------------------
 #include "eapp_utils.h"
 #include "string.h"
 #include "edge_call.h"
@@ -20,12 +16,11 @@
 unsigned long ocall_print_string(char* string);
 
 
-
+char *my_strcpy(char *strDest, const char *strSrc);
 
 
 int main(){
-	unsigned long size = 0x1000;
- 	//printf("pagesize is 0x%lx\n", size);	
+	unsigned long size = (0x1000)*5;
 	char * mmap_region = (char*)SYSCALL(
 	           SYS_mmap,
 	           0x2000000000,
@@ -36,10 +31,14 @@ int main(){
 	          );
 
 	  	if (mmap_region == MAP_FAILED) {
-		  //printf("!! MMAP 1 FAILED !!\n");
+		    ocall_print_string("mmap into DRAM region fail");
                 } else {
-		  // printf("!! MMAP 1 SUCCESS !!\n");
+		    ocall_print_string("mmap into DRAM region successful");
+
         }
+		my_strcpy(mmap_region, "SUCCESSFULLY WROTE INTO DRAM REGION");
+		ocall_print_string(mmap_region);
+
 
 	    char * mmap_region2 = (char*)SYSCALL(
 	           SYS_mmap,
@@ -51,29 +50,37 @@ int main(){
 	          );
 
   	if (mmap_region2 == MAP_FAILED) {
-                   // printf("!! MMAP 2 FAILED !!\n");
+                  ocall_print_string("mmap into NVM region failed");
+
                 } else {
-                    //  printf("!! MMAP 2 SUCCESS !!\n");
+	   	  ocall_print_string("mmap into NVM region successful");
+
         }
+                my_strcpy(mmap_region2, "SUCCESSFULLY WROTE INTO NVM REGION");
+                ocall_print_string(mmap_region2);
+
 
   	int unmap_result = SYSCALL(SYS_munmap, mmap_region, size, -1,0,0);
  	 if (unmap_result != 0) {
-    	//printf("Could not munmap mmap region 1\n");
+		 ocall_print_string("munmap of DRAM failed");
+    
   	} else {
-      	//printf("Successfully unmapped region 1\n");
+		ocall_print_string("munmap of DRAM success");
+      
   	}
 
 
 	  	
 	int unmap_result2 = SYSCALL(SYS_munmap, mmap_region2, size, -2,0,0);
  	 if (unmap_result2 != 0) {
-    	//printf("Could not munmap mmap region 2\n");
+                 ocall_print_string("munmap of NVM failed");
   	} else {
-      	//printf("Successfully unmapped region 2\n");
+                 ocall_print_string("munmap of NVM sucess");
+
   	}
 
-
-
+	my_strcpy(mmap_region, "SUCCESSFULLY WROTE INTO DRAM REGION");
+	my_strcpy(mmap_region2, "SUCCESSFULLY WROTE INTO NVM REGION");
 
   EAPP_RETURN(0);
 }
@@ -83,3 +90,16 @@ unsigned long ocall_print_string(char* string){
   ocall(OCALL_PRINT_STRING, string, strlen(string)+1, &retval ,sizeof(unsigned long));
   return retval;
 }
+
+char *my_strcpy(char *strDest, const char *strSrc)
+                {
+                    if (strSrc == NULL || strDest == NULL)
+                        return NULL;
+
+                    char *dest = strDest;
+
+                    while ((*strDest++ = *strSrc++) != '\0');
+
+                    return dest;
+                }
+
